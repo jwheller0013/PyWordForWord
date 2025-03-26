888520cda36df89a20a6285f56f6c2bccf89edf4
jwheller0013 <jwheller0013@yahoo.com>
2025-03-26 19:21:27 -0400

    Framing of program

---
+++ word_for_word.py
@@ -0,0 +1,28 @@
+
+
+class TextProcessing():
+    def __init__(self):
+        pass
+
+    #reads the contents of a file, line by line, and creates a String object
+    #making sure all the newlines are preserved.
+    def file_to_string(filename):
+        pass
+
+    #count the number of characters in a file, number of words, number of lines
+    #Returns an tuple with the number of lines, words and characters
+    def wc(str: input):
+        pass
+
+    #word count. words in the string, produce a dictionary with (str word, int numOfTimes)
+    def word_frequency(str: input):
+        pass
+
+    #collects the frequency of each letter within the input
+    #dictionary with letters as the key, number of occurences as value.
+    def letter_frequency(str: input):
+        pass
+
+    #numberOfOccurences / totalNumberOfWords
+    def frequency(word):
+        pass
